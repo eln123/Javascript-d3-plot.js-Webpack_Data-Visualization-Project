1eb4365379a5110e57f7ce80c1d81f1681d548e1
Ethan <elnair122@gmail.com>
2022-08-19 14:41:12 -0400

    d3IsWorking

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { converter } from "../../csvConverter";
+import { dThreeFunction } from "./dThree";
 
 /**
  * COMPONENT
@@ -13,16 +14,21 @@ export default class Home extends React.Component {
     };
   }
   componentDidMount() {
-    const url =
+    const urlC02Emissions =
+      "https://raw.githubusercontent.com/2206-capstone-npm-CEED/Dashboard_All_Datas/main/CO2_emissions_per_ton";
+    const urlPopulation =
       "https://raw.githubusercontent.com/open-numbers/ddf--gapminder--population/master/ddf--datapoints--population--by--country--year.csv";
     const fileStr = converter(url, (results) => {
       this.setState({ data: results.data });
+      dThreeFunction();
     });
   }
   render() {
-    console.log(this.state);
     return (
       <div>
+        <svg fill="black" width="500px" height="500px">
+          <circle cx="50" cy="50" r="50" fill="black"></circle>
+        </svg>
         <table>
           <thead>
             <tr>
