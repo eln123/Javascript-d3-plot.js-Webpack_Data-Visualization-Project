bbea1a518680382f0c140b6e4c2ef8e4c8555409
Ethan <elnair122@gmail.com>
2022-09-02 17:07:40 -0400

    friday507

--- client/components/Facet.js
+++ client/components/Facet.js
@@ -8,15 +8,65 @@ export class PlotFacet extends React.Component {
   constructor(props) {
     super(props);
     this.state = this.state = {
+      minYear: "",
+      maxYear: "",
       regions: ["americas", "europe", "asia"],
       years: ["1900", "1930"],
     };
+    this.selectRegion = this.selectRegion.bind(this);
     this.helper = this.helper.bind(this);
-    this.selectCountry = this.selectCountry.bind(this);
+    this.changeMinYear = this.changeMinYear.bind(this);
+    this.changeMaxYear = this.changeMaxYear.bind(this);
+    this.updateMinYear = this.updateMinYear.bind(this);
+    this.updateMaxYear = this.updateMaxYear.bind(this);
+  }
+  updateMinYear(evt) {
+    evt.preventDefault;
+    this.setState({
+      ...this.state,
+      minYear: evt.target.value,
+    });
+  }
+  updateMaxYear(evt) {
+    this.setState({
+      ...this.state,
+      maxYear: evt.target.value,
+    });
+  }
+  changeMinYear(evt) {
+    evt.preventDefault();
+
+    this.setState({
+      ...this.state,
+      years: [String(this.state.minYear), this.state.years[1]],
+    });
+  }
+  changeMaxYear(evt) {
+    evt.preventDefault();
+    this.setState({
+      ...this.state,
+      years: [this.state.years[0], String(this.state.maxYear)],
+    });
+  }
+  selectRegion(evt) {
+    if (evt.target.checked === true) {
+      this.setState({
+        ...this.state,
+        regions: [...this.state.regions, String(evt.target.name)],
+      });
+    } else {
+      const newState = {
+        ...this.state,
+        regions: this.state.regions.filter(
+          (region) => region !== String(evt.target.name)
+        ),
+      };
+
+      this.setState(newState);
+    }
   }
 
   helper() {
-    console.log(this.props);
     const lifeExpectancyArr = this.props.data.lifeExpectancy;
     const incomeArr = this.props.data.incomePerPerson;
     const populationArr = this.props.data.population;
@@ -90,10 +140,55 @@ export class PlotFacet extends React.Component {
   }
   render() {
     if (this.props.data.lifeExpectancy) {
-      let combined = this.helper();
+      let data = this.helper();
+      let datas = this.props.data.lifeExpectancy
+        .map((obj) => obj.region)
+        .filter((obj) => obj);
+      let regionArr = [];
+      for (let i = 0; i < datas.length; i++) {
+        let region = datas[i];
+        if (!regionArr.includes(region)) {
+          regionArr.push(region);
+        }
+      }
+      let one = data.filter((obj) => +obj.lifeExpectancy < 5);
+
       return (
-        <div>
-          <PlotFigure options={plotFuncFacet(combined)} />
+        <div className="confine">
+          <PlotFigure options={plotFuncFacet(data)} />
+          <form onSubmit={this.changeMinYear}>
+            <label htmlFor="minYear"> MinYear </label>
+            <input
+              name="minYear"
+              placeholder="minYear"
+              onChange={this.updateMinYear}
+            />
+            <button type="submit"> Update </button>
+          </form>
+
+          <form onSubmit={this.changeMaxYear}>
+            <label htmlFor="maxYear"> MaxYear </label>
+            <input
+              name="value"
+              placeholder="maxYear"
+              onChange={this.updateMaxYear}
+            />
+            <button type="submit"> Update </button>
+          </form>
+          <fieldset className="checkBoxes">
+            <label htmlFor="checkBox">
+              {regionArr.map((region, index) => (
+                <div key={index}>
+                  <input
+                    type="checkbox"
+                    name={region}
+                    onClick={this.selectRegion}
+                  />
+                  {region}
+                </div>
+              ))}
+            </label>
+          </fieldset>
         </div>
       );
     } else {
